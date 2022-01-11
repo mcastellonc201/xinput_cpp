e1e288736ede6cd035462bd65f2b1f0e2abfa65d
mcastellonc201 <castellonmichael@gmail.com>
2022-01-10 18:06:30 -0800

    Update xinput.cpp

--- xinput.cpp
+++ xinput.cpp
@@ -5,16 +5,20 @@
 
 int main()
 {
-	XINPUT_STATE state;
+    XINPUT_STATE state;
 
-	while (true)
-	{
-		DWORD Result = XInputGetState(0, &state);
-		if (state.Gamepad.wButtons & XINPUT_GAMEPAD_A)
-		{
-			std::cout << "Button: A\n";
-		}
-		Sleep(150);
-	}
-	return 0;
+    while (true)
+    {
+        DWORD Result = XInputGetState(0, &state);
+        if (Result == ERROR_SUCCESS)
+        {
+            if (state.Gamepad.wButtons && XINPUT_GAMEPAD_A)
+            {
+                std::cout << "Button: A" << std::endl;
+            }
+        }
+        Sleep(150);
+    }
+    return 0;
 }
+//https://docs.microsoft.com/en-us/windows/win32/api/xinput/ns-xinput-xinput_gamepad
