de8b26633e065e92c890ed4a59d8c2e4b48086b9
mcastellonc201 <castellonmichael@gmail.com>
2022-01-19 15:06:18 -0800

    Update xkeystroke.cpp

--- xkeystroke.cpp
+++ xkeystroke.cpp
@@ -6,18 +6,14 @@
 int main()
 {
     XINPUT_KEYSTROKE stroke;
-
-    while (true)
+    do
     {
         DWORD Result = XInputGetKeystroke(0, 0, &stroke);
-        if (Result == ERROR_SUCCESS)
+        if ((stroke.VirtualKey == VK_PAD_A) && (stroke.Flags & XINPUT_KEYSTROKE_KEYDOWN))
         {
-            if ((stroke.VirtualKey == VK_PAD_A) && (stroke.Flags & XINPUT_KEYSTROKE_KEYDOWN))
-            {
-                std::cout << "Button: A" << std::endl;
-            }
+            printf("A\n");
         }
-    }
+    } while ( !(GetAsyncKeyState(VK_ESCAPE) & 0x01) );
     return 0;
 }
 //https://docs.microsoft.com/en-us/windows/win32/api/xinput/ns-xinput-xinput_keystroke
